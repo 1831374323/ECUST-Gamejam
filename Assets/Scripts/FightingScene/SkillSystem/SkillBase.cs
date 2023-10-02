753f5261b59eaa216b0a0b42f9307015fead5531
SiYao <1831374323@qq.com>
2023-10-02 16:06:01 +0800

    skill基类更新

--- Assets/Scripts/FightingScene/SkillSystem/SkillBase.cs
+++ Assets/Scripts/FightingScene/SkillSystem/SkillBase.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using FightingScene.UnitSystem;
 
 namespace EcustGamejam
 {
     public class SkillBase : ScriptableObject
     {
         public string m_name;
-        public virtual void SkillApply()
+        public virtual void SkillApply(UnitMono skillUser,UnitMono target)
         {
 
         }
 
-        public virtual void SkillDisable()
+        public virtual void SkillDisable(UnitMono skillUser, UnitMono target)
         {
 
         }
