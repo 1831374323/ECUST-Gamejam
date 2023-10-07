a507df15d1d742d84338ba96d6e9046775595cc5
可恶的亿万富翁 <384040021@qq.com>
2023-10-07 15:52:28 +0800

    更改卦位ui图，增加显示信息
    用小部分拼起来重新做了position的ui，添加显示信息但尚未完成

---
+++ Assets/Scripts/FightingScene/UI/show.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class show : MonoBehaviour
+{
+
+    public void Click()
+    {
+        gameObject.SetActive(true);
+    }
+}
