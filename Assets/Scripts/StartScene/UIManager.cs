c4889106aab883b1f3a29d857e3f697559b35569
SoltidueMarquez <2630388173@qq.com>
2023-10-09 00:16:56 +0800

    音乐替换&开始界面制作者名单

--- Assets/Scripts/StartScene/UIManager.cs
+++ Assets/Scripts/StartScene/UIManager.cs
@@ -9,10 +9,11 @@ namespace StartScene
 {
     public class UIManager : SingletonBase<UIManager>
     {
+        [SerializeField, Tooltip("制作人员UI")] private GameObject makerUI;
+        
         /// <summary>
         /// 开始游戏函数
         /// </summary>
-        ///
         public void EnterGame()
         {
             AudioManager.instance.PlaySound(0);
@@ -23,6 +24,14 @@ namespace StartScene
         {
             PlayerPrefs.DeleteAll();
             EnterGame();
+        }
+
+        /// <summary>
+        /// 打开关闭制作者UI函数
+        /// </summary>
+        public void OpenCloseMakerUI()
+        {
+            makerUI.SetActive(!makerUI.activeSelf);
         }
     }
 }
