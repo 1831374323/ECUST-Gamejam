a4d45acff1b77ae4ee7135ca737d6c53bd7b0b0e
SiYao <1831374323@qq.com>
2023-10-09 22:00:28 +0800

    完成吸血技能

--- Assets/Scripts/FightingScene/SkillSystem/PositionAtkSkill/KanAtkSkill.cs
+++ Assets/Scripts/FightingScene/SkillSystem/PositionAtkSkill/KanAtkSkill.cs
@@ -9,9 +9,13 @@ namespace EcustGamejam
     {
         [SerializeField]
         List<int> atkValue = new List<int>();
+        public List<int> stealHp= new List<int>();
         protected override float GetAtkValue(int level)
         {
-            //Debug.Log($"¿²½øÐÐ{level}½×¹¥»÷");
+            int hpValue;
+            int shieldValue;
+            base.m_target.GetComingDamage(atkValue[level], out hpValue, out shieldValue);
+            m_skillUser.SetHp((int)(hpValue * stealHp[level] * 0.01f));
             
             return atkValue[level];
         }
