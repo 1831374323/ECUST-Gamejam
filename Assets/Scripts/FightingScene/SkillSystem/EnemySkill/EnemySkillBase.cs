341871b5171d032e8dbab2e7fbbc69a97842ff0a
可恶的亿万富翁 <384040021@qq.com>
2023-10-10 15:04:01 +0800

    修改数值调整界面
    修改数值调整界面

---
+++ Assets/Scripts/FightingScene/SkillSystem/EnemySkill/EnemySkillBase.cs
@@ -0,0 +1,21 @@
+using FightingScene.UnitSystem;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EcustGamejam
+{
+    public class EnemySkillBase : ScriptableObject
+    {
+        public string m_name;
+        public virtual void SkillApply(UnitMono skillUser, UnitMono target)
+        {
+            FightingUIManager.Instance.UpDateBehaviourText($"µ–»À Õ∑≈¡À{m_name}\r\n");
+        }
+
+        public virtual void SkillDisable(UnitMono skillUser, UnitMono target)
+        {
+
+        }
+    }
+}
